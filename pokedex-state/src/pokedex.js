b15b527980e98e0f6ac929c7b8441d829e1ebfab
Mauricio Homma <mhhomma@gmail.com>
2020-07-14 14:49:53 -0300

    exercise-code

---
+++ pokedex-state/src/pokedex.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import pokemonList from './data';
+import Pokemons from './pokemon'
+
+class pokedex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokemonIndex: 0,
+      filterType: 'Fire'
+    }
+  }
+
+  handleClick = (listLength) => {
+    this.nextPokemon(this.state, listLength);
+  }
+
+  nextPokemon = (state, listLength) => {
+    this.setState({
+      pokemonIndex: state.pokemonIndex + 1 >= listLength ? 0 : state.pokemonIndex + 1
+    })
+  }
+
+  setFilter = (state, filter) => {
+    this.setState({
+      pokemonIndex: state.pokemonIndex,
+      filterType: filter
+    })
+  }
+
+  render() {
+    const filteredPokemons = pokemonList
+          .filter((pokemon) => this.state.filterType === pokemon.type)
+
+    const {name, type, image, averageWeight } = filteredPokemons[this.state.pokemonIndex];
+   
+   return (
+     <div>
+      <Pokemons
+        name={name}
+        type={type}
+        image={image}
+        weight={averageWeight.value}
+        unit={averageWeight.measurementUnit}
+        />
+        <button onClick={() => this.handleClick(filteredPokemons.length)}>Proximo</button>
+        <button onClick={() => this.setFilter(this.state, 'Fire')}>Fire</button>
+        <button onClick={() => this.setFilter(this.state, 'Psychic')}>Psychic</button>
+    </div>
+    );
+  }
+}
+
+export default pokedex;
---
+++ pokedex-state/src/pokemon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class pokemon extends React.Component {
+  render() {
+    return (
+    <section className={`pokecontainer ${this.props.type}`}>
+      <div> 
+      <p>{this.props.name}</p>
+      <p>{this.props.type}</p>
+      <p>{`${this.props.weight} ${this.props.unit}`}</p>
+      </div>
+      <img src={this.props.image}/>
+    </section>
+    );
+  }
+}
+
+export default pokemon;

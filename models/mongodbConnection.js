6013c6c4d8734bd84e43116a870479a1054ddd5e
Roz <roz@rjmunhoz.me>
2020-11-24 16:50:04 -0300

    Change mongodb connection

--- models/mongodbConnection.js
+++ models/mongodbConnection.js
@@ -3,8 +3,15 @@ const { MongoClient } = require('mongodb');
 const URI = 'mongodb://localhost:27017/live_lecture_27_1';
 const DB_NAME = 'live_lecture_27_1';
 
-module.exports = async (collectionName) =>
-  MongoClient.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then((connection) => connection.db(DB_NAME).collection(collectionName));
+let connection = null;
+
+module.exports = async function (collectionName) {
+  connection =
+    connection ||
+    (await MongoClient.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }));
+
+  return connection.db(DB_NAME).collection(collectionName);
+};

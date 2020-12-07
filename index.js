2c51d23e8c04241f58cf43b6eb903e9f18fda052
fsrocha.dev <contato.frankrocha@gmail.com>
2020-12-07 17:28:50 -0300

    setando outra variavel

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const SERVER_ENV = process.env.SERVER_ENV || 'Não achei';
+
 app.get('/', (req, res) => {
-	res.send('Hello Trybe');
+	res.send(`Rodando em:${SERVER_ENV}`);
 });
 
 const PORT = process.env.PORT || 3000;
 
+//50811
 app.listen(PORT, () => {
 	console.log(`Escutando aqui ${PORT}`);
 });
